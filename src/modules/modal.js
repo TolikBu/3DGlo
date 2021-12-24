f2c7c880311296d33144ad17fd5362d07c867165
Tolikbu <tolikelement@gmail.com>
2021-12-24 13:37:13 +0500

    fix modal.js menu .js

--- src/modules/modal.js
+++ src/modules/modal.js
@@ -3,48 +3,30 @@ const modal = () => {
   const modalContent = document.querySelector(".popup-content");
   const buttons = document.querySelectorAll(".popup-btn");
   const modalClose = modal.querySelector(".popup-close");
-
   const width = document.documentElement.clientWidth;
 
-  let count = '';
+  let count = 0;
   let interval;
-
-  modalContent.style.top = "-380px";
 
   buttons.forEach((btn) => {
     btn.addEventListener("click", () => {
       modal.style.display = "block";
       const modalAnimation = () => {
-        count += 2 + "px";
-        
-        modalContent.style.top = `${0 + count}`;
-        if (width > 768 && count < 100) {
-          requestAnimationFrame(modalAnimation);
-        } 
+        count++;
+        interval = requestAnimationFrame(modalAnimation);
+        if (count < 16 && width > 768) {
+          modalContent.style.top = count * 6 + "px";
+        } else {
+          cancelAnimationFrame(interval);
+        }
       };
-      
-      //     console.log(count);
-      //     modalContent.style.top = count + "px";
-      //   } 
-      // const modalAnimation = () => {
-      //   count++;
-      //   interval = requestAnimationFrame(modalAnimation);
-      //   if (width > 768 && count < 100) {
-      //     console.log(count);
-      //     modalContent.style.top = count + "px";
-      //   } else {
-      //     cancelAnimationFrame(interval);
-      //     modalContent.style.top = "10%";
-      //   }
-      // };
-      // modalAnimation();
+        modalAnimation();
     });
   });
 
   modalClose.addEventListener("click", () => {
     modal.style.display = "none";
     count = 0;
-    console.log(count);
   });
 };
 
