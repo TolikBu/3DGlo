2b50db6cc16fcb449866fd178b600000d56b0f63
TolikBu <tolikelement@gmail.com>
2021-12-24 08:46:15 +0500

    fix lesson20

--- src/modules/menu.js
+++ src/modules/menu.js
@@ -3,23 +3,21 @@ const menu = () => {
   const menu = document.querySelector("menu");
   const closeBtn = menu.querySelector(".close-btn");
   const menuItems = menu.querySelectorAll("ul>li>a");
-  const btnScroll = document.querySelector("main>a");
-  const array = [...menuItems, btnScroll];
-  console.log(array);
+  const btnScroll = document.getElementById("service");
+  const service = document.getElementById("service-block");
 
-  const handleMenu = () => {
+  const handleMenu = () => { 
     menu.classList.toggle("active-menu");
   };
 
   menuBtn.addEventListener("click", handleMenu);
   closeBtn.addEventListener("click", handleMenu);
-  menuItems.forEach((menuItem) => menuItem.addEventListener("click", handleMenu));
 
-  array.forEach(link => {
+  menuItems.forEach((link) => {
     link.addEventListener("click", (event) => {
       event.preventDefault();
+      handleMenu();
       const ID = event.target.getAttribute("href").substr(1);
-
       document.getElementById(ID).scrollIntoView({
         behavior: "smooth",
         block: "start",
@@ -27,7 +25,13 @@ const menu = () => {
     });
   });
 
-
+  btnScroll.addEventListener("click", (event) => {
+    event.preventDefault();
+    service.scrollIntoView({
+      behavior: "smooth",
+      block: "start",
+    });
+  });
 };
 
 export default menu;
--- src/modules/modal.js
+++ src/modules/modal.js
@@ -6,31 +6,45 @@ const modal = () => {
 
   const width = document.documentElement.clientWidth;
 
-  let count = 10;
+  let count = '';
   let interval;
+
+  modalContent.style.top = "-380px";
 
   buttons.forEach((btn) => {
     btn.addEventListener("click", () => {
       modal.style.display = "block";
       const modalAnimation = () => {
-        count++;
-        interval = requestAnimationFrame(modalAnimation);
-
-        if (count > 10 && count < 100) {
-          modalContent.style.opacity = "0." + count;
-        } else {
-          cancelAnimationFrame(interval);
-        }
+        count += 2 + "px";
+        
+        modalContent.style.top = `${0 + count}`;
+        if (width > 768 && count < 100) {
+          requestAnimationFrame(modalAnimation);
+        } 
       };
-      if (width > 768) {
-        modalAnimation();
-      }
+      
+      //     console.log(count);
+      //     modalContent.style.top = count + "px";
+      //   } 
+      // const modalAnimation = () => {
+      //   count++;
+      //   interval = requestAnimationFrame(modalAnimation);
+      //   if (width > 768 && count < 100) {
+      //     console.log(count);
+      //     modalContent.style.top = count + "px";
+      //   } else {
+      //     cancelAnimationFrame(interval);
+      //     modalContent.style.top = "10%";
+      //   }
+      // };
+      // modalAnimation();
     });
   });
 
   modalClose.addEventListener("click", () => {
     modal.style.display = "none";
-    count = "10";
+    count = 0;
+    console.log(count);
   });
 };
 
