076ae9f1dd74f904ba4ba52fdc88d8ecebb407f0
TolikBu <tolikelement@gmail.com>
2021-12-25 16:04:06 +0500

    work valid name phone

--- src/index.js
+++ src/index.js
@@ -1,8 +1,12 @@
 import timer from "./modules/timer";
 import menu from "./modules/menu";
 import modal from "./modules/modal";
+import calc from "./modules/calc";
+import formValid from "./modules/formValid";
 
 
-timer("28 december 2021");
+timer("31 december 2021");
 menu();
 modal();
+calc();
+formValid();
