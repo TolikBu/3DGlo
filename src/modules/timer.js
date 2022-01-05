f8c197d15656a934fcc2a3ff006e68775d763d4d
TolikBu <tolikelement@gmail.com>
2022-01-05 20:32:24 +0500

    fix timer.js: add days off

--- src/modules/timer.js
+++ src/modules/timer.js
@@ -1,35 +1,42 @@
 const timer = (deadline) => {
-  const timerHours = document.getElementById("timer-hours");
-  const timerMinutes = document.getElementById("timer-minutes");
-  const timerSeconds = document.getElementById("timer-seconds");
+  const timer = document.getElementById('timer');
+  const timerDays = document.createElement('span');
+  const timerHours = document.getElementById('timer-hours');
+  const timerMinutes = document.getElementById('timer-minutes');
+  const timerSeconds = document.getElementById('timer-seconds');
+
+  timerDays.classList = 'timer-days';
+  timer.prepend(timerDays);
 
   const getTimeRemaining = () => {
     let dateStop = new Date(deadline).getTime();
     let dateNow = new Date().getTime();
     let timeRemaining = (dateStop - dateNow) / 1000;
-    // let days = Math.floor(timeRemaining / 60 / 60 / 24);
-    let hours = Math.floor(timeRemaining / 60 / 60);
+    let days = Math.floor(timeRemaining / 60 / 60 / 24);
+    let hours = Math.floor((timeRemaining / 60 / 60) % 24);
     let minutes = Math.floor((timeRemaining / 60) % 60);
     let seconds = Math.floor(timeRemaining % 60);
 
-    return { timeRemaining, hours, minutes, seconds };
+    return { timeRemaining, days, hours, minutes, seconds };
   };
 
-   const zero = function (item) {
-     return (item = ("0" + item).slice(-2));
-   };
+  const zero = function (item) {
+    return (item = ('0' + item).slice(-2));
+  };
 
   const updateClock = () => {
     let getTime = getTimeRemaining();
 
-    timerHours.textContent = getTime.hours;
+    timerDays.textContent = getTime.days + ' дн.';
+    timerHours.textContent = `${zero(getTime.hours)}`;
     timerMinutes.textContent = `${zero(getTime.minutes)}`;
     timerSeconds.textContent = `${zero(getTime.seconds)}`;
 
     if (getTime.timeRemaining < 0) {
-      timerHours.textContent = "00";
-      timerMinutes.textContent = "00";
-      timerSeconds.textContent = "00";
+      timerDays.textContent = '00';
+      timerHours.textContent = '00';
+      timerMinutes.textContent = '00';
+      timerSeconds.textContent = '00';
     }
   };
   setInterval(updateClock, 1000);
