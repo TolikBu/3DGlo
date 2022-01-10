bb36662ea9fc5b85cd5a543f784909127963ef01
Tolikbu <tolikelement@gmail.com>
2022-01-10 13:43:49 +0500

    fix formValidate.js sendForm.js: fix validate

--- src/modules/formValid.js
+++ src/modules/formValid.js
@@ -1,34 +1,45 @@
 const formValid = () => {
-  const input = document.querySelectorAll('input');
-  const formName = document.querySelectorAll('.form-name');
-  const formPhone = document.querySelectorAll('.form-phone');
-  const formEmail = document.querySelectorAll('.form-email');
-  const formMess = document.querySelector('.mess');
+  const input = document.querySelectorAll("input");
+  const formName = document.querySelectorAll(".form-name");
+  const formPhone = document.querySelectorAll(".form-phone");
+  const formEmail = document.querySelectorAll(".form-email");
+  const formMess = document.querySelector(".mess");
 
   input.forEach((e) => {
     for (let i of formName) {
-      i.addEventListener('input', (e) => {
-        e.target.value = e.target.value.replace(/[^а-яА-Я\s]/gi, '');
+      i.addEventListener("input", (e) => {
+        e.target.value = e.target.value.replace(/[^а-яА-Я\s]/gi, "");
       });
     }
     for (let i of formPhone) {
-      i.addEventListener('input', (e) => {
-        e.target.value = e.target.value.replace(/[^\d()-]/gi, '');
+      i.addEventListener("input", (e) => {
+        e.target.value = e.target.value.replace(/[^\d()-]/gi, "");
       });
     }
     for (let i of formEmail) {
-      i.addEventListener('input', (e) => {
-        e.target.value = e.target.value.replace(/[^a-zA-Z0-9\@\-\_\.\~\!\*\']/gi, '');
+      i.addEventListener("input", (e) => {
+        e.target.value = e.target.value.replace(/[^a-zA-Z0-9\@\-\_\.\~\!\*\']/gi, "");
       });
     }
   });
-  formMess.addEventListener('input', (e) => {
-    e.target.value = e.target.value.replace(/[^а-яА-Я\?\!\_\.\:\;\'\-\s\d]/gi, '');
+  formMess.addEventListener("input", (e) => {
+    e.target.value = e.target.value.replace(/[^а-яА-Я\?\!\_\.\:\;\'\-\s\d]/gi, "");
   });
 
-  input.forEach((e) => {
-    
-  })
+  formName.forEach((i) => {
+    i.addEventListener("blur", (e) => {
+      e.target.value = e.target.value.trim();
+
+      if (e.target.classList.contains("form-name") && e.target.value) {
+        e.target.value = e.target.value
+          .split(/\ +/)
+          .map((word) => word[0].toUpperCase() + word.slice(1).toLowerCase())
+          .join(" ");
+      }
+
+      e.target.value = e.target.value;
+    });
+  });
 };
 
 export default formValid;
--- src/modules/sendForm.js
+++ src/modules/sendForm.js
@@ -1,24 +1,30 @@
 const sendForm = ({ formId, someElem = [] }) => {
   const form = document.getElementById(formId);
-  const statusBlock = document.createElement('div');
-  const loadText = 'Загрузка...';
-  const errorText = 'Ошибка...';
-  const successText = 'Наш менеджер с вами свяжется!';
+  const statusBlock = document.createElement("div");
+  const loadText = "Загрузка...";
+  const errorText = "Ошибка...";
+  const successText = "Наш менеджер с вами свяжется!";
+
+  statusBlock.style.color = 'white';
+
+  let formElements = form.querySelectorAll("input");
 
   const validate = (list) => {
     let success = true;
 
     list.forEach((input) => {
-      const formElement = form.querySelectorAll('input');
+      formElements = form.querySelectorAll("input");
       let reg = /^([A-Za-z0-9_\-\.])+\@([A-Za-z0-9_\-\.])+\.([A-Za-z]{2,4})$/gi;
 
       if (input.classList.contains('success')) {
         success = false;
-      } else if (formElement[0] < 3) {
+      } 
+
+      if (formElements[0].value.length < 3) {
         success = false;
-      } else if (reg.test(formElement[1].value) == false) {
+      } else if (reg.test(formElements[1].value) == false) {
         success = false;
-      } else if (formElement[2].value.length < 18) {
+      } else if (formElements[2].value.length < 18) {
         success = false;
       }
     });
@@ -27,23 +33,23 @@ const sendForm = ({ formId, someElem = [] }) => {
   };
 
   const sendData = (data) => {
-    return fetch('https://jsonplaceholder.typicode.com/posts', {
-      method: 'POST',
+    return fetch("https://jsonplaceholder.typicode.com/posts", {
+      method: "POST",
       body: JSON.stringify(data),
       headers: {
-        'Content-Type': 'application/json',
+        "Content-Type": "application/json",
       },
     }).then((res) => res.json());
   };
 
   const submitForm = () => {
-    const formElements = form.querySelectorAll('input');
+    formElements = form.querySelectorAll("input");
     const formData = new FormData(form);
     const formBody = {};
 
-
     statusBlock.textContent = loadText;
     form.append(statusBlock);
+
 
     formData.forEach((val, key) => {
       formBody[key] = val;
@@ -52,9 +58,9 @@ const sendForm = ({ formId, someElem = [] }) => {
     someElem.forEach((elem) => {
       const element = document.getElementById(elem.id);
 
-      if (elem.type === 'block') {
+      if (elem.type === "block") {
         formBody[elem.id] = element.textContent;
-      } else if (elem.type === 'input') {
+      } else if (elem.type === "input") {
         formBody[elem.id] = element.value;
       }
     });
@@ -63,31 +69,33 @@ const sendForm = ({ formId, someElem = [] }) => {
       sendData(formBody)
         .then((data) => {
           statusBlock.textContent = successText;
-         
-          
+          setInterval(() => {
+            statusBlock.remove(form);
+          }, 2000);
+
           formElements.forEach((input) => {
             input.value = '';
           });
+          return;
         })
         .catch((error) => {
           statusBlock.textContent = errorText;
-          //  setInterval(() => {
-          //    form.delete(statusBlock);
-          //  }, 3000);
+          setInterval(() => {
+            statusBlock.remove(form);
+          }, 2000);
         });
     } else {
-      alert('Данные не валидны: имя должно быть не короче 3 симвовлов, E-mail в формате xxxx@yyyy.zzz, номер телефона не короче 11 цифр');
+      alert("Данные не валидны: имя должно быть не короче 3 симвовлов, E-mail в формате xxxx@yyyy.zzz, номер телефона не короче 11 цифр");
       statusBlock.textContent = errorText;
     }
-
   };
 
   try {
     if (!form) {
-      throw new Error('Верните форму на место');
+      throw new Error("Верните форму на место");
     }
 
-    form.addEventListener('submit', (e) => {
+    form.addEventListener("submit", (e) => {
       e.preventDefault();
 
       submitForm();
