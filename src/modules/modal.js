a5eedf063bf095e480717422315d2696a7ef4786
TolikBu <tolikelement@gmail.com>
2022-01-11 22:51:12 +0500

    fix second check

--- src/modules/modal.js
+++ src/modules/modal.js
@@ -4,30 +4,30 @@ const modal = () => {
   const modal = document.querySelector(".popup");
   const modalContent = document.querySelector(".popup-content");
   const buttons = document.querySelectorAll(".popup-btn");
-  const width = document.documentElement.clientWidth;
-  
+
   let count = 0;
 
   buttons.forEach((btn) => {
     btn.addEventListener("click", () => {
       modal.style.display = "block";
-      if (width > 768) {
-      animate({
-        duration: 300,
-        timing(timeFraction) {
-          return Math.pow(timeFraction, 2);
-        },
-        draw(progress) {
-          modalContent.style.top = progress * 10 + '%';
-        }
-      });
-    }
+      if (document.documentElement.clientWidth > 768) {
+        animate({
+          duration: 300,
+          timing(timeFraction) {
+            return timeFraction;
+          },
+          draw(progress) {
+            modalContent.style.top = progress * 10 + '%';
+          },
+        });
+      }
     });
   });
 
   modal.addEventListener("click", (e) => {
     if (!e.target.closest(".popup-content") || e.target.classList.contains("popup-close")) {
       modal.style.display = "none";  
+      modalContent.style.top = '';
     }
     count = 0;
   });
